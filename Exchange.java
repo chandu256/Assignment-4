8f8d477187c3fab4fe9b17831ba0833183ac823c
chandu256 <81565889+chandu256@users.noreply.github.com>
2021-04-23 22:25:42 +0530

    Add files via upload

---
+++ Exchange.java
@@ -0,0 +1,18 @@
+ class genericexchange
+{
+public static <E>void printArray(E[]inputArray,int i,int i)
+{
+for(E element:inputArray)
+{
+System.out.printf("%s",element);
+}
+System.out.println();
+}
+public static void main (String[]args,int i)
+{
+Integer[] iList={3,7,4,9,5};
+Swap(iList,2,4)
+for(Integer i:iList)
+System.out.println(i+",");
+}
+}
