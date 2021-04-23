a44041c91f934ee9beed0dfaf78ba7fd0c399fe1
chandu256 <81565889+chandu256@users.noreply.github.com>
2021-04-23 22:24:45 +0530

    Add files via upload

---
+++ replace.java
@@ -0,0 +1,10 @@
+class replace
+{
+public static void main (String[] args)
+{
+String str="lucky";
+String new_str=str.replace('u','o');
+System.out.println("original String:"+str);
+System.out.println("new String:"+new_str);
+}
+}
